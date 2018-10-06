02366a97d224c8cf48cc663c5b3659a70082bd4f
Park Jongchan <itspjc@gmail.com>
2018-10-06 17:23:33 +0900

    Update requests, BeautifulSoup Example

--- requests_example.py
+++ requests_example.py
@@ -1,4 +1,21 @@
 import requests
-url = "http://www.yahoo.co.jp"
+import csv
+from bs4 import BeautifulSoup
+url = "https://www.beeradvocate.com/beer/"
 response = requests.get(url)
-print(response.text)
+text = response.text
+
+soup = BeautifulSoup(text, "html.parser")
+
+# a = soup.find("h6")
+# print(a)
+
+with open('beer_list.csv', 'w') as output:
+    writer = csv.writer(output, delimiter=',', lineterminator='\n')
+    writer.writerow(["name", "rate"])
+
+    for item in soup.find_all("div", {"id": "rating_fullview_container"}):
+        beer_name = item.h6.a.get_text()
+        beer_rate = item.find("span", {"class": "BAscore_norm"}).get_text()
+        writer.writerow([beer_name, beer_rate])
+        print([beer_name, beer_rate])
--- selenium_example.py
+++ selenium_example.py
@@ -1,3 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+path_chrome_driver = "./chromedriver"
+url = "https://www.beeradvocate.com/"
+
+driver = webdriver.Chrome(path_chrome_driver)
+driver.get(url)
 
 
-
+sleep(5)
+driver.close()
